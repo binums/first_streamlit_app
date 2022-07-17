dfa62facb9b5120867b9848327aff9418e0b34f2
binums <binums56@gmail.com>
2022-07-17 19:43:35 +0530

    Fix: fecthone to fetchall

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ my_cur = my_cnx.cursor()
 # streamlit.text("Hello from Snowflake:")
 
 my_cur.execute("SELECT * from fruit_load_list")
-my_data_row = my_cur.fetchone()
+my_data_row = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 
 streamlit.dataframe(my_data_row)
