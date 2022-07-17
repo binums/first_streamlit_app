6d6c3f9e636d4a5ace720d5ac7f9f07d6e211a71
binums <binums56@gmail.com>
2022-07-17 10:31:12 +0530

    Feat: add pandas and render fruit list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title("My Mom's New Healthy Diner")
 
@@ -7,3 +8,6 @@ streamlit.text("🥣 Omega 3 & Blueberry Oatmeal")
 streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
 streamlit.text("🐔 Hard-Boiled Free-Range Egg")
 streamlit.text("🥑🍞 Avocado Toast")
+
+myFruitList = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+streamlit.dataframe(myFruitList)
