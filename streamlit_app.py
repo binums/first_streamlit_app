f1bb1bf5475f6f0192eacabee5c5b05ebd9ff4af
binums <binums56@gmail.com>
2022-07-17 10:38:32 +0530

    Fix: title to header

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,6 @@ streamlit.text("🥑🍞 Avocado Toast")
 
 myFruitList = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 
-streamlit.title("🍌 🥭 Build Your OwnFruit Smoothie 🥝 🍇")
+streamlit.header("🍌 🥭 Build Your OwnFruit Smoothie 🥝 🍇")
 streamlit.multiselect("Pick some fruits:", list(myFruitList.index))
 streamlit.dataframe(myFruitList)
